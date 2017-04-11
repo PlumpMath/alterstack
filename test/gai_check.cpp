99d49641a45e9eedd4813e99005731871dadb15e
Alex Syrnikov <san@masterspline.net>
2017-04-11 15:41:02 +0300

    update ext/cpu_utils submodule

--- test/gai_check.cpp
+++ test/gai_check.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Alexey Syrnikov <san@masterspline.net>
+ * Copyright 2015,2017 Alexey Syrnikov <san@masterspline.net>
  *
  * This file is part of Alterstack.
  *
@@ -26,7 +26,7 @@
 
 #include <iostream>
 
-#include "CpuUtils.hpp"
+#include "cpu_utils.hpp"
 
 int main()
 {
